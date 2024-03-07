96b645d9309c4b5ac7bc840ffe81c041faae15d7
Kened Silva Felix <ksfelix1@stefanini.com>
2024-03-06 23:47:36 -0300

    feat: adicionado porta variavel ao servidor

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,14 +7,17 @@ const app = express();
 app.use(express.json());
 // Middleware para habilitar o CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Ou especifique as origens permitidas, por exemplo: 'http://example.com'
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header("Access-Control-Allow-Origin", "*"); // Ou especifique as origens permitidas, por exemplo: 'http://example.com'
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
-app.use(routes)
+app.use(routes);
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log("HTTP server is running!!!");
 });
